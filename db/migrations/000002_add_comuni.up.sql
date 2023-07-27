afbb64618b508b1c99891cc23967ef77e7cb5459
Eliseo Martelli <me@eliseomartelli.it>
2023-07-27 11:51:55 +0200

    feat: add more towns

---
+++ db/migrations/000002_add_comuni.up.sql
@@ -0,0 +1,10 @@
+INSERT INTO towns(id, name, feed_url)
+	VALUES
+		("ssquisq", "Santo Stefano Quisquina", "https://www.comune.santostefanoquisquina.ag.it/feed/"),
+		("rsg", "Rocca San Giovanni", "https://www.comuneroccasangiovanni.it/feed/"),
+		("castelfr", "Castel Frentano", "https://www.comunedicastelfrentano.it/feed/"),
+		("treglio", "Treglio", "https://www.comune.treglio.chieti.it/po/elenco_news_rss.php?tags=2&area=H"),
+		("paglieta", "Paglieta", "https://comune.paglieta.ch.it/webpaglieta/feed/"),
+		("casoli", "Casoli", "https://www.comune.casoli.ch.it/web/feed/"),
+		("casalbordino", "Casalbordino", "https://www.casalbordino.info/feed/")
+;
